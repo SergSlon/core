52f7c0aea20c01085b47c0672042eb25925a719f
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2012-06-17 01:04:05 +0200

    Travis should now work correctly for Fuel\Core as well.

--- phpunitbootstrap.php
+++ phpunitbootstrap.php
@@ -2,9 +2,19 @@
 
 if (file_exists($file = __DIR__.'/../../autoload.php'))
 {
+	$vendorDir = __DIR__.'/../../';
 	include $file;
 }
 else
 {
-	include 'vendor/autoload.php';
+	$vendorDir = __DIR__.'/vendor/';
+	include './vendor/autoload.php';
 }
+
+$env = new Fuel\Kernel\Environment();
+$env->init(array(
+	'name'         => 'test',
+	'path'         => $vendorDir,
+	'namespaces'   => require $vendorDir.'composer/autoload_namespaces.php',
+	'environments' => array(),
+));
---
+++ resources/tests/ErrorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fuel\Core;
+
+/**
+ * @backupGlobals  disabled
+ */
+class ErrorTest extends \PHPUnit_Framework_TestCase
+{
+	public function testShowNonFatal()
+	{
+		$this->markTestIncomplete('Unfinished');
+	}
+}
